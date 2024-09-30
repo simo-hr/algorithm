1452b2c4d8ba56d899fbb8335a917d9bd5578a90
github xxbbxx825 <xxbbxx825@gmail.com>
2024-09-30 11:31:22 +0900

    Reviewed selection sort

--- introduction/sort/selection.py
+++ introduction/sort/selection.py
@@ -1,20 +1,20 @@
 from typing import List
 
 
-def selection_sort(numbers: List[int]) -> List[int]:
-    len_nums = len(numbers)
+def selection_sort(nums: List[int]) -> List[int]:
+    len_nums = len(nums)
     for i in range(len_nums):
-        min_index = i
-        for j in range(i + 1, len_nums - 1):
-            if numbers[j] < numbers[min_index]:
-                min_index = j
-        numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
-    return numbers
+        temp_index = i
+        for j in range(i + 1, len_nums):
+            if nums[j] < nums[temp_index]:
+                temp_index = j
+        nums[i], nums[temp_index] = nums[temp_index], nums[i]
+    return nums
 
 
 if __name__ == "__main__":
     import random
 
-    nums = [1, 7, 3, 2, 8, 5]
-    nums = [random.randint(0, 1000) for _ in range(10)]
+    nums = [2, 7, 3, 1, 8, 5]
+    # nums = [random.randint(0, 1000) for _ in range(10)]
     print(selection_sort(nums))
