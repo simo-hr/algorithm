99c0674528e1825258510b1c7b13051cd94ce9f8
github xxbbxx825 <xxbbxx825@gmail.com>
2024-09-30 12:04:03 +0900

    Reviewed insertion sort

--- introduction/sort/insertion.py
+++ introduction/sort/insertion.py
@@ -1,19 +1,31 @@
 from typing import List
 
 
-def insertion_sort(numbers: List[int]) -> List[int]:
-    len_numbers = len(numbers)
-    for i in range(1, len_numbers):
-        temp = numbers[i]
+def insertion_sort(nums: List[int]) -> List[int]:
+    len_nums = len(nums)
+    for i in range(1, len_nums):
+        temp = nums[i]
         j = i - 1
-        while j >= 0 and numbers[j] > temp:
-            numbers[j + 1] = numbers[j]
-            print(numbers)
+        while j >= 0 and temp < nums[j]:
+            nums[j + 1] = nums[j]
             j -= 1
-        numbers[j + 1] = temp
-    return numbers
+        nums[j + 1] = temp
+
+    return nums
+
+
+# def insertion_sort(nums: List[int]) -> List[int]:
+#     len_nums = len(nums)
+#     for i in range(1, len_nums):
+#         j = i - 1
+#         while j >= 0:
+#             if nums[j + 1] < nums[j]:
+#                 nums[j + 1], nums[j] = nums[j], nums[j + 1]
+#             j -= 1
+#     return nums
 
 
 if __name__ == "__main__":
-    nums = [1, 7, 3, 2, 8, 5]
+    nums = [7, 3, 1, 2, 8, 5]
+    # nums = [3, 7, 1, 2, 8, 5]
     print(insertion_sort(nums))
